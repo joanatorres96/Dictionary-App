a21b6dadf0dd5ae43ab3a271b6a6dc1da9f55d47
Joana Torres <joanatorres96@gmail.com>
2022-01-14 00:07:46 +0000

    added image api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,15 +2,21 @@ import React , {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(){
 let [word, setWord] = useState (null);
 let [results, setResults] = useState (null);
+let [photos, setPhotos] = useState(null);
 
 function handleResponse(response){
 console.log(response.data[0]);
 setResults(response.data[0]);
 };
+
+function handlePexelsResponse(response){
+ setPhotos(response.data.photos);
+}
 
 
 function searchWord(event){
@@ -18,7 +24,12 @@ function searchWord(event){
 
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     axios.get(apiUrl).then(handleResponse);
-}
+
+
+    let pexelsApiKey="563492ad6f917000010000015fade4ded159433eaf771056ad00383a";
+    let pexelsApiUrl=`https://api.pexels.com/v1/search?query=https://api.pexels.com/v1/search?query=${word}&per_page=4`;
+    axios.get(pexelsApiUrl, {headers: {"Authorization" : `Bearer ${pexelsApiKey}`}}).then(handlePexelsResponse);
+  }
 function updateWord(event){
     setWord(event.target.value);
 }
@@ -35,6 +46,9 @@ return (
 <div className="col-sm-6 text-left my-5">
 <Results results={results} />
 </div>
+<div className="col-sm-6 text-left my-5">
+  <Photos photos={photos} />
+  </div>
 <div className='text-center fs-5'>
         <p> <a href='https://github.com/joanatorres96/Dictionary-App'title='Github Link' target={"_blank"} rel={"noreferrer"} className='link'>Open-source code</a>{" by "}<a href='https://cranky-poincare-a3701c.netlify.app/' target={"_blank"} rel={"noreferrer"} className='link'>Joana Torres</a>. </p>
       </div>
---
+++ src/Photos.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Photos(props){
+if (props.photos){
+    return(
+        <div className="Photos">
+            <div className="row mt-3">
+            {props.photos.map(function (photo, index){
+                return(
+                    <div className="col-md-6" key={index}>
+                        <a href={photo.src.original} target={"_blank"} rel={"noreferrer"}>
+                   <img src={photo.src.tiny} className="img-fluid images"/>
+                        </a>
+                   </div>
+                );
+            })}
+            </div>
+        </div>
+    );
+}else{
+    return null;
+}
+
+
+
+}
